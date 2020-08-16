9186d9e60427ef9a60d6a7f05cdc1358eeda2340
Matthew Piedra <matthewpiedra@Matthews-MacBook-Air-2.local>
2020-08-15 23:05:24 -0400

    Added question4

--- src/questions/index.js
+++ src/questions/index.js
@@ -1,3 +1,4 @@
 export { default as Question1 } from './Question1.js';
 export { default as Question2 } from './Question2.js';
-export { default as Question3 } from './Question3';
+export { default as Question3 } from './Question3.js';
+export { default as Question4 } from './Question4.js';
